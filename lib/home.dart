88fa59b476169ed2e2360f723c0b9c415cfd4f10
Ahmed Arigbabu <medwonuola@gmail.com>
2021-04-18 15:41:33 +0100

    Headache

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:duo_names/favorites.dart';
 import 'package:duo_names/model.dart';
 import 'package:flutter/material.dart';
 
@@ -7,12 +8,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Names namesProvider;
+
+  void fav() async {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) => Favorites()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    namesProvider = DuoName.of(context).names;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Name Generator"),
+        actions: [
+          IconButton(icon: Icon(Icons.favorite_outlined), onPressed: fav)
+        ],
+      ),
+      body: namesProvider.names == null
+          ? Center(child: CircularProgressIndicator())
+          : RefreshIndicator(
+              onRefresh: namesProvider.getNames,
+              child: ListView.separated(
+                itemBuilder: (BuildContext context, int index) =>
+                    _itemUI(index, namesProvider.names),
+                itemCount: namesProvider.names.length,
+                separatorBuilder: (BuildContext context, int index) => Divider(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+    );
+  }
+
+  _itemUI(int index, List builder) {
+    String _item = builder[index];
+    return ListTile(
+      title: Text(_item),
+      trailing: IconButton(
+          icon: Icon(
+            namesProvider.isFav(_item)
+                ? Icons.favorite_outlined
+                : Icons.favorite_border_outlined,
+            color: namesProvider.isFav(_item) ? Colors.red : Colors.grey,
+          ),
+          onPressed: () {
+            setState(() {
+              namesProvider.addFavorites(_item);
+            });
+          }),
+    );
   }
 }
-
 
 class DuoName extends InheritedWidget {
   final Widget child;
